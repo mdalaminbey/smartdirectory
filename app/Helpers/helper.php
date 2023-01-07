f102fa759a016618f4cca5fbd9e6564d3b4723ce
MD AL AMIN <mdalaminbey@gmail.com>
2023-01-07 17:07:52 +0600

    added allowed html kses

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -8,3 +8,110 @@ function smart_directory_post_type()
 {
     return Application::$config['post_types']['directory'];
 }
+
+function smart_directory_kses_allowed()
+{
+    return [
+        'a'          => [
+            'class'  => [],
+            'href'   => [],
+            'rel'    => [],
+            'title'  => [],
+            'target' => []
+        ],
+        'abbr'       => [
+            'title' => []
+        ],
+        'b'          => [],
+        'blockquote' => [
+            'cite' => []
+        ],
+        'cite'       => [
+            'title' => []
+        ],
+        'code'       => [],
+        'del'        => [
+            'datetime' => [],
+            'title'    => []
+        ],
+        'dd'         => [],
+        'div'        => [
+            'class' => [],
+            'title' => [],
+            'style' => []
+        ],
+        'dl'         => [],
+        'dt'         => [],
+        'em'         => [],
+        'h1'         => [
+            'class' => []
+        ],
+        'h2'         => [
+            'class' => []
+        ],
+        'h3'         => [
+            'class' => []
+        ],
+        'h4'         => [
+            'class' => []
+        ],
+        'h5'         => [
+            'class' => []
+        ],
+        'h6'         => [
+            'class' => []
+        ],
+        'i'          => [
+            'class' => []
+        ],
+        'img'        => [
+            'alt'    => [],
+            'class'  => [],
+            'height' => [],
+            'src'    => [],
+            'width'  => []
+        ],
+        'li'         => [
+            'class' => []
+        ],
+        'ol'         => [
+            'class' => []
+        ],
+        'p'          => [
+            'class' => []
+        ],
+        'q'          => [
+            'cite'  => [],
+            'title' => []
+        ],
+        'span'       => [
+            'class' => [],
+            'title' => [],
+            'style' => []
+        ],
+        'iframe'     => [
+            'width'       => [],
+            'height'      => [],
+            'scrolling'   => [],
+            'frameborder' => [],
+            'allow'       => [],
+            'src'         => []
+        ],
+        'strike'     => [
+            'class' => []
+        ],
+        'br'         => [
+            'class' => []
+        ],
+        'strong'     => [
+            'class' => []
+        ],
+        'ul'         => [
+            'class' => []
+        ],
+        'ins'        => [
+            'class'    => [],
+            'datetime' => []
+        ]
+    ];
+}
