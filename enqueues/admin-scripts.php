3c871da717d06134d3daf7d819875de79854f7e4
MD AL AMIN <mdalaminbey@gmail.com>
2023-01-09 02:02:13 +0600

    prepared release 1.0.0

--- enqueues/admin-scripts.php
+++ enqueues/admin-scripts.php
@@ -4,4 +4,4 @@ use SmartDirectory\Bootstrap\System\Utils\Common;
 
 defined( 'ABSPATH' ) || exit;
 
-wp_enqueue_style( 'smart-directory-tailwind', Common::asset( 'css/app.css' ), [], time() );
+wp_enqueue_style( 'smart-directory-tailwind', Common::asset( 'css/app.css' ), [], Common::version() );
--- enqueues/frontend-scripts.php
+++ enqueues/frontend-scripts.php
@@ -4,7 +4,7 @@ use SmartDirectory\Bootstrap\System\Utils\Common;
 
 defined( 'ABSPATH' ) || exit;
 
-wp_enqueue_script( 'smart-directory-pagination-js', Common::asset( 'js/simplePagination.js' ), ['jquery'], time() );
-wp_enqueue_script( 'smart-directory-js', Common::asset( 'js/app.js' ), ['jquery'], time() );
-wp_enqueue_style( 'smart-directory-pagination-css', Common::asset( 'css/simplePagination.css' ), [], time() );
-wp_enqueue_style( 'smart-directory-tailwind', Common::asset( 'css/app.css' ), [], time() );
+wp_enqueue_script( 'smart-directory-pagination-js', Common::asset( 'js/simplePagination.js' ), ['jquery'], Common::version() );
+wp_enqueue_script( 'smart-directory-js', Common::asset( 'js/app.js' ), ['jquery'], Common::version() );
+wp_enqueue_style( 'smart-directory-pagination-css', Common::asset( 'css/simplePagination.css' ), [], Common::version() );
+wp_enqueue_style( 'smart-directory-tailwind', Common::asset( 'css/app.css' ), [], Common::version() );
